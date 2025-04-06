3bbef51d0eded20ed803de70e04ce7da7b512ee1
roblesdotdev <httpsnip@gmail.com>
2025-04-06 19:17:30 -0300

    migrate to biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { ViteMinifyPlugin as minify } from 'vite-plugin-minify'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import compression from 'vite-plugin-compression'
+import { ViteMinifyPlugin as minify } from 'vite-plugin-minify'
 
+// biome-ignore lint/nursery/noProcessEnv: get port from env
 const DEFAULT_PORT = Number(process.env.PORT || 3000)
 
 export default defineConfig({
