f3804c6558cef272592258fdec01bca815250c2e
roblesdotdev <httpsnip@gmail.com>
2025-04-08 13:18:39 -0300

    preload sprite

--- src/root.tsx
+++ src/root.tsx
@@ -1,28 +1,28 @@
-import { lazy } from 'react'
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router'
 import HomeRoute from '~/routes/home'
-
-const NotFoundRoute = lazy(() => import('./routes/not-found.tsx'))
+import NotFoundRoute from './routes/not-found.tsx'
 
 function Layout() {
   return (
-    <div className="flex min-h-screen flex-col p-4">
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <footer className="py-12 text-center font-light">
-        <p className="text-neutral-600">
-          by{' '}
-          <a
-            target="_blank"
-            href="https://github.com/tinogasta-labs"
-            rel="noreferrer noopener"
-          >
-            Tinogasta Labs
-          </a>
-        </p>
-      </footer>
-    </div>
+    <>
+      <div className="flex min-h-screen flex-col p-4">
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <footer className="py-12 text-center font-light">
+          <p className="text-neutral-600">
+            by{' '}
+            <a
+              target="_blank"
+              href="https://github.com/tinogasta-labs"
+              rel="noreferrer noopener"
+            >
+              Tinogasta Labs
+            </a>
+          </p>
+        </footer>
+      </div>
+    </>
   )
 }
 
