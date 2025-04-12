59ebcb6690725a0ddde7873e64a107ca5ffbf2a0
roblesdotdev <httpsnip@gmail.com>
2025-04-12 17:56:21 -0300

    404 page

--- src/routes/_404.tsx
+++ src/routes/_404.tsx
@@ -3,8 +3,13 @@ import { IconArrow } from '~/components/ui/icon'
 export default function NotFoundRoute() {
   return (
     <section className="mx-auto flex max-w-7xl flex-1 flex-col items-center px-6 py-40">
-      <h1 className="text-3xl">404: Not Found</h1>
-      <p className="mt-4">It's gone :(</p>
+      <span className="text-indigo-700">404</span>
+      <h1 className="text-center text-3xl leading-tight font-medium text-neutral-800 sm:text-5xl sm:leading-snug">
+        Ups! Page Not Found
+      </h1>
+      <p className="mt-4 text-center">
+        The page you're looking for doesn't exist or may have been moved.
+      </p>
       <div className="mt-8">
         <a href="/" className="inline-flex items-center gap-2 underline">
           <IconArrow displayDirection="left" />
